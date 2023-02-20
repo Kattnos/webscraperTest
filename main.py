ad1d07073f8155af390634f9e9b54e43ec4fecd1
Simon Hagelin <kattnoskanal@gmail.com>
2023-02-20 17:20:52 +0100

    fixed bug with other types of articles

--- main.py
+++ main.py
@@ -5,15 +5,23 @@ import time
 import webbrowser
 
 def clicking_article(selectedArticle):
-    if 1 <= selectedArticle <= 8 and isinstance(selectedArticle, int):
+    if 1 <= int(selectedArticle) <= 8:
+        article = int(selectedArticle) + 1
         driver.find_element(by=By.XPATH,
-                            value=f'//*[@id="yDmH0d"]/c-wiz/div/main/c-wiz/div[2]/c-wiz/c-wiz[{selectedArticle + 1}]/c-wiz/div/article').click()
+                            value=f'//*[@id="yDmH0d"]/c-wiz/div/main/c-wiz/div[2]/c-wiz/c-wiz[{article}]/c-wiz/div/article').click()
         time.sleep(5)
         driver.switch_to.window(driver.window_handles[1])
         url = driver.current_url
         webbrowser.open_new(url)
     else:
         print('Input does not correspond to an article')
+
+
+def ask_for_input():
+    selectedArticle = input("Select article to open or type 'exit' to exit")
+    if selectedArticle != 'exit':
+        clicking_article(selectedArticle)
+
 
 ser = Service(r"C:\Users\05SIHAB\Documents\chromedriver")
 op = webdriver.ChromeOptions()
@@ -23,8 +31,15 @@ driver.get(url)
 refuseCookiesButton = '//*[@id="yDmH0d"]/c-wiz/div/div/div/div[2]/div[1]/div[3]/div[1]/div[1]/form[1]/div/div/button'
 driver.find_element(by=By.XPATH, value=refuseCookiesButton).click()
 
-for x in range(2, 5):
+for x in range(2, 32):
      news_path = f'//*[@id="yDmH0d"]/c-wiz/div/main/c-wiz/div[2]/c-wiz/c-wiz[{x}]/c-wiz/div/article/h4'
-     link = driver.find_element(by=By.XPATH, value=news_path)
-     visibleInt = (x - 1)
-     print(f'{visibleInt}: {link.text}')
+     try:
+         link = driver.find_element(by=By.XPATH, value=news_path)
+     except:
+         news_path = f'//*[@id="yDmH0d"]/c-wiz/div/main/c-wiz/div[2]/c-wiz/c-wiz[{x}]/c-wiz/article/div[1]/div[2]/div/h4'
+     finally:
+        link = driver.find_element(by=By.XPATH, value=news_path)
+        visibleInt = (x - 1)
+        print(f'{visibleInt}: {link.text}')
+
+ask_for_input()
