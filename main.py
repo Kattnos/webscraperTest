67380785dca8714122cef1986bd9ce9b8c8699cb
Kattnos <kattnoskanal@gmail.com>
2023-02-23 19:21:57 +0100

    started adding features for article reader

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ import time
 import webbrowser
 
 def clicking_article(selectedArticle):
-    if 1 <= int(selectedArticle) <= 8:
+    if 1 <= int(selectedArticle) <= 30:
         article = int(selectedArticle) + 1
         driver.find_element(by=By.XPATH,
                             value=f'//*[@id="yDmH0d"]/c-wiz/div/main/c-wiz/div[2]/c-wiz/c-wiz[{article}]/c-wiz/div/article').click()
-        time.sleep(5)
         driver.switch_to.window(driver.window_handles[1])
         url = driver.current_url
         webbrowser.open_new(url)
+        ask_for_input()
     else:
         print('Input does not correspond to an article')
 
@@ -22,8 +22,15 @@ def ask_for_input():
     if selectedArticle != 'exit':
         clicking_article(selectedArticle)
 
+class article:
+    def __init__(self, title, company , content, button):
+        self.title = title
+        self.company = company
+        self.content = content
+        self.button = button
 
-ser = Service(r"C:\Users\05SIHAB\Documents\chromedriver")
+#ser = Service(r"C:\Users\05SIHAB\Documents\chromedriver")
+ser = Service(r"C:\Dev\Python\webscraperTest\chromedriver")
 op = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=ser, options=op)
 url = 'https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx1YlY4U0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US%3Aen'
@@ -31,15 +38,29 @@ driver.get(url)
 refuseCookiesButton = '//*[@id="yDmH0d"]/c-wiz/div/div/div/div[2]/div[1]/div[3]/div[1]/div[1]/form[1]/div/div/button'
 driver.find_element(by=By.XPATH, value=refuseCookiesButton).click()
 
+articles = []
+
 for x in range(2, 32):
      news_path = f'//*[@id="yDmH0d"]/c-wiz/div/main/c-wiz/div[2]/c-wiz/c-wiz[{x}]/c-wiz/div/article/h4'
      try:
-         link = driver.find_element(by=By.XPATH, value=news_path)
+         title = driver.find_element(by=By.XPATH, value=news_path)
      except:
          news_path = f'//*[@id="yDmH0d"]/c-wiz/div/main/c-wiz/div[2]/c-wiz/c-wiz[{x}]/c-wiz/article/div[1]/div[2]/div/h4'
      finally:
-        link = driver.find_element(by=By.XPATH, value=news_path)
-        visibleInt = (x - 1)
-        print(f'{visibleInt}: {link.text}')
+        title = driver.find_element(by=By.XPATH, value=news_path)
+
+        buttonPath = f'//*[@id="yDmH0d"]/c-wiz/div/main/c-wiz/div[2]/c-wiz/c-wiz[{x}]/c-wiz/div/article'
+        button = driver.find_element(by=By.XPATH, value=buttonPath)
+        button.click()
+        driver.switch_to.window(driver.window_handles[1])
+        url = driver.current_url
+        driver.close()
+
+        articles.append(article(title.text,'dsfs','fsdfs' , button))
+
+        visibleInt = x-1
+        listInt = x-2
+
+        print(f'{visibleInt}: {articles[listInt].title}')
 
 ask_for_input()
