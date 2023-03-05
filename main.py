acd01f98bc045bd2aab647556b3460397c19ec32
Kattnos <kattnoskanal@gmail.com>
2023-03-05 16:11:43 +0100

    fixed url finder

--- main.py
+++ main.py
@@ -40,21 +40,23 @@ driver.find_element(by=By.XPATH, value=refuseCookiesButton).click()
 
 articles = []
 
-for x in range(2, 32):
+for x in range(2, 22):
      news_path = f'//*[@id="yDmH0d"]/c-wiz/div/main/c-wiz/div[2]/c-wiz/c-wiz[{x}]/c-wiz/div/article/h4'
+     buttonPath = f'//*[@id="yDmH0d"]/c-wiz/div/main/c-wiz/div[2]/c-wiz/c-wiz[{x}]/c-wiz/div/article'
      try:
          title = driver.find_element(by=By.XPATH, value=news_path)
      except:
          news_path = f'//*[@id="yDmH0d"]/c-wiz/div/main/c-wiz/div[2]/c-wiz/c-wiz[{x}]/c-wiz/article/div[1]/div[2]/div/h4'
+         buttonPath = f'//*[@id="yDmH0d"]/c-wiz/div/main/c-wiz/div[2]/c-wiz/c-wiz[{x}]/c-wiz/article/div[1]/div[1]/a'
      finally:
         title = driver.find_element(by=By.XPATH, value=news_path)
-
-        buttonPath = f'//*[@id="yDmH0d"]/c-wiz/div/main/c-wiz/div[2]/c-wiz/c-wiz[{x}]/c-wiz/div/article'
         button = driver.find_element(by=By.XPATH, value=buttonPath)
+
         button.click()
         driver.switch_to.window(driver.window_handles[1])
         url = driver.current_url
         driver.close()
+        driver.switch_to.window(driver.window_handles[0])
 
         articles.append(article(title.text,'dsfs','fsdfs' , button))
 
