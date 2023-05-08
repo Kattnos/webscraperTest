5df575de887aa8ce8cd4c08b62de1e65a1908cd5
Simon Hagelin <hagelin05@gmail.com>
2023-05-08 19:35:27 +0200

    improvement

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
 import webbrowser
+import tkinter as tk
 
 file = open('companyNames.txt')
 companyFile = file.readlines()
+
+root=tk.Tk()
+root.geometry('900x700')
 
 companyURL = []
 companyFormatted = []
@@ -18,51 +22,14 @@ print(companyURL)
 def clicking_article(selectedArticle):
     article = int(selectedArticle) - 1
     webbrowser.open_new(articles[article].url)
-    ask_for_input()
-
 
 def print_url(selectedArticle):
     article = int(selectedArticle) - 1
     print(articles[article].url)
-    ask_for_input()
 
 def print_company(selectedArticle):
     article = int(selectedArticle) - 1
     print(articles[article].company)
-    ask_for_input()
-
-
-def ask_for_input():
-    selectedInput = input("Enter a command or type 'help'")
-
-    if selectedInput == 'help':
-        print('Available commands: \n articleNumber.open \n articleNumber.company \n articleNumber.url \n exit')
-        ask_for_input()
-    if selectedInput == 'exit':
-        return
-
-    subCommand = selectedInput.rpartition('.')[2]
-    selectedArticle = selectedInput.rpartition('.')[0]
-
-    try:
-        selectedInt = int(selectedArticle)
-    except:
-        print('Unknown command')
-        ask_for_input()
-    if 5 >= selectedInt >= 1:
-        match subCommand:
-            case 'open':
-                clicking_article(selectedInt)
-            case 'url':
-                print_url(selectedInt)
-            case 'company':
-                print_company(selectedInt)
-            case _:
-                print('Unknown command')
-        ask_for_input()
-    else:
-        print('Article not found')
-        ask_for_input()
 
 class article:
     def __init__(self, title, company , content, button, url):
@@ -71,6 +38,47 @@ class article:
         self.content = content
         self.button = button
         self.url = url
+
+articles = []
+def refresh_articles():
+    for x in range(2, 7):
+        news_path = f'//*[@id="yDmH0d"]/c-wiz/div/main/c-wiz/div[2]/c-wiz/c-wiz[{x}]/c-wiz/div/article/h4'
+        buttonPath = f'//*[@id="yDmH0d"]/c-wiz/div/main/c-wiz/div[2]/c-wiz/c-wiz[{x}]/c-wiz/div/article'
+        try:
+            title = driver.find_element(by=By.XPATH, value=news_path)
+        except:
+            news_path = f'//*[@id="yDmH0d"]/c-wiz/div/main/c-wiz/div[2]/c-wiz/c-wiz[{x}]/c-wiz/article/div[1]/div[2]/div/h4'
+            buttonPath = f'//*[@id="yDmH0d"]/c-wiz/div/main/c-wiz/div[2]/c-wiz/c-wiz[{x}]/c-wiz/article/div[1]/div[1]/a'
+        finally:
+            title = driver.find_element(by=By.XPATH, value=news_path)
+            button = driver.find_element(by=By.XPATH, value=buttonPath)
+
+            # button.click()
+            # driver.switch_to.window(driver.window_handles[1])
+            # url = driver.current_url
+            # driver.close()
+            # driver.switch_to.window(driver.window_handles[0])
+
+            for string in companyURL:
+                searchableURL = url.rpartition('1')[0]
+                if searchableURL.find(string) != -1:
+                    searchInt = companyURL.index(string)
+                    company = companyFormatted[searchInt].rstrip()
+                    print(company)
+                    break
+                else:
+                    company = 'Unknown'
+
+            articles.append(article(title.text, company, 'fsdfs', button, url))
+
+            visibleInt = x - 1
+            listInt = x - 2
+
+            print(f'{visibleInt}: {articles[listInt].title}')
+            print(len(articles))
+            print(len(titleText))
+            titleText[listInt].config(text=articles[listInt].title)
+
 
 ser = Service(r"C:\Users\05SIHAB\Documents\chromedriver")
 #ser = Service(r"C:\Dev\Python\webscraperTest\chromedriver")
@@ -83,41 +91,38 @@ driver.get(url)
 refuseCookiesButton = '//*[@id="yDmH0d"]/c-wiz/div/div/div/div[2]/div[1]/div[3]/div[1]/div[1]/form[1]/div/div/button'
 driver.find_element(by=By.XPATH, value=refuseCookiesButton).click()
 
-articles = []
+articleSelectField = tk.Frame(root)
+titleText = []
+titleButton = []
 
-for x in range(2, 7):
-     news_path = f'//*[@id="yDmH0d"]/c-wiz/div/main/c-wiz/div[2]/c-wiz/c-wiz[{x}]/c-wiz/div/article/h4'
-     buttonPath = f'//*[@id="yDmH0d"]/c-wiz/div/main/c-wiz/div[2]/c-wiz/c-wiz[{x}]/c-wiz/div/article'
-     try:
-         title = driver.find_element(by=By.XPATH, value=news_path)
-     except:
-         news_path = f'//*[@id="yDmH0d"]/c-wiz/div/main/c-wiz/div[2]/c-wiz/c-wiz[{x}]/c-wiz/article/div[1]/div[2]/div/h4'
-         buttonPath = f'//*[@id="yDmH0d"]/c-wiz/div/main/c-wiz/div[2]/c-wiz/c-wiz[{x}]/c-wiz/article/div[1]/div[1]/a'
-     finally:
-        title = driver.find_element(by=By.XPATH, value=news_path)
-        button = driver.find_element(by=By.XPATH, value=buttonPath)
+for article in articles:
+   titleText.append(tk.Label(articleSelectField))
+   titleButton.append(tk.Button(articleSelectField, text='Read Article'))
 
-        button.click()
-        driver.switch_to.window(driver.window_handles[1])
-        url = driver.current_url
-        driver.close()
-        driver.switch_to.window(driver.window_handles[0])
+inputField = tk.Frame(root)
+programTitle = tk.Label(inputField, text='Retrieve and read articles from Google News', font=('Arial', 16))
+refreshButton = tk.Button(inputField, text='Refresh', font=('Arial', 14), command=refresh_articles)
 
-        for string in companyURL:
-            searchableURL = url.rpartition('1')[0]
-            if searchableURL.find(string) != -1:
-                searchInt = companyURL.index(string)
-                company = companyFormatted[searchInt].rstrip()
-                print(company)
-                break
-            else:
-                company = 'Unknown'
+outputFieldFrame = tk.Frame(root)
+outputField = tk.Text(outputFieldFrame, font=('Arial', 12))
 
-        articles.append(article(title.text, company, 'fsdfs', button, url))
+for title in titleText:
+    title.grid(column=0,row=(titleText.index(title)), pady=3)
+for button in titleButton:
+    button.grid(column=1, row=(titleButton.index(button)),pady=3)
 
-        visibleInt = x-1
-        listInt = x-2
+programTitle.pack(pady=10)
+refreshButton.pack(pady=5)
 
-        print(f'{visibleInt}: {articles[listInt].title}')
+inputField.pack(pady=10,padx=2)
+articleSelectField.pack(pady=5,padx=5)
+outputFieldFrame.pack(pady=20,padx=10)
+outputField.pack(side =tk.LEFT, fill = tk.BOTH)
 
-ask_for_input()
+scroll = tk.Scrollbar(outputFieldFrame, orient='vertical', command=outputField.yview)
+outputField.config(yscrollcommand=scroll.set)
+scroll.pack(side=tk.RIGHT, fill='y')
+
+root.mainloop()
+
+refresh_articles()
